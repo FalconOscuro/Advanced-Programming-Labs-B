21b58b8a3971b1cf72fa31c4de775c379b434072
Peter Garrod <p.jlgarrod@gmail.com>
2023-02-10 18:47:42 +0000

    Provided extra user feedback

--- 01-Good-Streaming/Good-Streaming.cpp
+++ 01-Good-Streaming/Good-Streaming.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main(int argn, char* argv[])
 {
+	cout << "Enter an int and a float separated by a ' '(space): ";
 	int i;
 	float f;
 	// take an int and float from the console - separated by a space, e.g. "1 6.7"
 	cin >> i >> f;
 
 	// Take a string from the console
+	cout << "Enter a string: ";
 	string s;
 	cin >> s;
 
