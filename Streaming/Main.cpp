37021fab259fdad951c4a1c1615bd4810aaeebbc
Peter Garrod <petergarrod@pop-os.localdomain>
2023-02-10 10:37:18 +0000

    Finished task 1

--- Streaming/Main.cpp
+++ Streaming/Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argn, char* argv[])
 {
 	int i;
 	float f;
 	// take an int and float from the console - separated by a space, e.g. "1 6.7"
-	std::cin >> i >> f;
+	cin >> i >> f;
+
+	// Take a string from the console
+	string s;
+	cin >> s;
+
 	// output the int and float to the console
-	std::cout << "i=" << i << ", f=" << f << std::endl;
+	cout << "i=" << i << ", f=" << f << endl;
+	// output string
+	cout << s << endl;
 }
