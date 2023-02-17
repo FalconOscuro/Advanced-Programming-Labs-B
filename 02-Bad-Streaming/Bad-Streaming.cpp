a415d1bec1e1c977844747312dde4ca2e1b783b7
Peter Garrod <p.jlgarrod@gmail.com>
2023-02-17 10:53:47 +0000

    Made task 2 input safe

--- 02-Bad-Streaming/Bad-Streaming.cpp
+++ 02-Bad-Streaming/Bad-Streaming.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 int main(int argn, char* argv[])
 {
+   cout << "Enter a character string: ";
+
    char c[5];
-   cin >> c;
-   cout << "c=" << c << endl;
+   cin.get(c, 5);
+   cout << "c = " << c << endl;
 }
